308db474530deb3bf6050921e1dc1ed8f013422e
Lucila Costa <lucilagdac@gmail.com>
2022-01-18 22:06:28 -0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+function clicar() {
+    let number1 = document.getElementById('number1')
+    if (number1.value < 1 || number.value > 100 || number1.value.length == 0) {
+        alert('Não foi possível efetuar essa operação!')
+    } else {
+        let listaNumeros = [ ]
+        number.value.push()
+    }
+}
