029875deaa8e7ed0339d857f88a5f64321c8b5f3
Lucila Costa <lucilagdac@gmail.com>
2022-02-09 21:16:43 -0300

    up

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function clicar() {
-    let number1 = document.getElementById('number1')
-    if (number1.value < 1 || number.value > 100 || number1.value.length == 0) {
-        alert('Não foi possível efetuar essa operação!')
+let num = getElementById('number') //input#number
+let list = getElementById('list') //select#list
+let resultado = getElementById('resultado') //div#resultado
+
+let valores = []
+
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        let listaNumeros = [ ]
-        number.value.push()
+        return false
     }
 }
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumber(num.value) && !inList(num.value, valores)) {
+        window.alert('ok')
+    } else {
+        alert('valor inválido ou já adicionado na lista.')
+    }
+}
+
+//number.value > 100 && number.value.length == 0)alert('Não foi possível efetuar essa operação!')
+//let listaNumeros = [ ] num.value.push()
